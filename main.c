7ed2287dc1872a661b2007dd0ffa53cc10b0d3fb
fosslinux <fosslinux@aussies.space>
2023-07-04 14:19:01 +1000

    Add + and - operators

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "M2libc/bootstrappable.h"
+
+#define FALSE 0
+#define TRUE 1
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -7,8 +12,34 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
+	char *a = calloc(strlen(argv[1]), sizeof(char));
+	strcpy(a, argv[1]);
+
 	fputs("int main() { return ", stdout);
-	fputs(argv[1], stdout);
+	char *b = a;
+	while (*b) {
+		while (*b >= 48 && *b <= 57) {
+			fputc(*b, stdout);
+			b += 1;
+		}
+    	if (*b == '+') {
+    		fputc('+', stdout);
+			b += 1;
+			continue;
+    	}
+		if (*b == '-') {
+			fputc('-', stdout);
+			b += 1;
+			continue;
+		}
+		if (!*b) {
+			b += 1;
+			break;
+		}
+
+		fputs("unexpected character\n", stderr);
+	}
+
 	fputs("; }\n", stdout);
 	return 0;
 }
