40abae8723cb06984462353d9503e15a26c273b8
fosslinux <fosslinux@aussies.space>
2023-07-08 09:46:50 +1000

    Add arrays of arrays

--- parse.c
+++ parse.c
@@ -129,7 +129,7 @@ Type *func_params(Token **rest, Token *tok, Type *ty) {
 }
 
 // type-suffix = "(" func-params
-//             | "[" num "]"
+//             | "[" num "]" type-suffix
 //             | ε
 Type *type_suffix(Token **rest, Token *tok, Type *ty) {
 	if (equal(tok, "(")) {
@@ -138,7 +138,8 @@ Type *type_suffix(Token **rest, Token *tok, Type *ty) {
 
 	if (equal(tok, "[")) {
 		int sz = get_number(tok->next);
-		*rest = skip(tok->next->next, "]");
+		tok = skip(tok->next->next, "]");
+		ty = type_suffix(rest, tok, ty);
 		return array_of(ty, sz);
 	}
 
