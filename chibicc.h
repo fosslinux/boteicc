71d48c64fabf90732b5b65be2af8073d9514a720
fosslinux <fosslinux@aussies.space>
2023-08-30 18:12:10 +1000

    Add cc1

--- chibicc.h
+++ chibicc.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "M2libc/bootstrappable.h"
 
 #define FALSE 0
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "chibicc.h"
 
+int opt_cc1;
+int opt_hash_hash_hash;
 char *opt_o;
 
 char *input_path;
@@ -13,6 +15,16 @@ void parse_args(int argc, char **argv) {
 	int i;
 	char *msg;
 	for (i = 1; i < argc; i += 1) {
+		if (!strcmp(argv[i], "-###")) {
+			opt_hash_hash_hash = TRUE;
+			continue;
+		}
+
+		if (!strcmp(argv[i], "-cc1")) {
+			opt_cc1 = TRUE;
+			continue;
+		}
+
 		if (!strcmp(argv[i], "--help")) {
 			usage(0);
 		}
@@ -65,11 +77,47 @@ FILE *open_file(char *path) {
 	return out;
 }
 
-int main(int argc, char **argv) {
+void run_subprocess(char **argv) {
+	// If -### is given, dump the subprocess command line.
+	if (opt_hash_hash_hash) {
+		fputs(argv[0], stderr);
+		int i;
+		for (i = 1; argv[i]; i += 1) {
+			fputc(' ', stderr);
+			fputs(argv[i], stderr);
+		}
+		fputc('\n', stderr);
+	}
+
+	int pid = fork();
+	if (pid == 0) {
+		// Child proc
+		execve(argv[0], argv, NULL);
+		fputs("exec failed: ", stderr);
+		fputs(argv[0], stderr);
+		fputc('\n', stderr);
+		exit(1);
+	}
+
+	// Wait for child process.
+	int status;
+	while (waitpid(pid, &status, 0) > 0) {}
+	if (status != 0) {
+		exit(1);
+	}
+}
+
+void run_cc1(int argc, char **argv) {
+	char **args = calloc(argc + 10, sizeof(char*));
+	memcpy(args, argv, argc * sizeof(char*));
+	args[argc] = "-cc1";
+	argc += 1;
+	run_subprocess(args);
+}
+
+void cc1(void) {
 	// Initialisation
 	initialize_types();
-
-	parse_args(argc, argv);
 
 	// Tokenize, parse.
 	Token *tok = tokenize_file(input_path);
@@ -78,6 +126,16 @@ int main(int argc, char **argv) {
 	// Traverse the AST, emitting assembly.
 	FILE *out = open_file(opt_o);
 	codegen(prog, out);
+}
 
+int main(int argc, char **argv) {
+	parse_args(argc, argv);
+
+	if (opt_cc1) {
+		cc1();
+		return 0;
+	}
+
+	run_cc1(argc, argv);
 	return 0;
 }
